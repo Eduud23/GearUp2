3b39678e8d4840c6020ec9d6941c2c1c4d32121d
JUde <judealberca3@gmail.com>
2025-04-02 18:53:17 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,7 +82,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
 
-
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
 
 
